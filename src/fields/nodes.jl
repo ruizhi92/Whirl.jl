2b15d1bf550095853c3acc82a6f9286e54181305
ruizhi92 <rzyang92@gmail.com>
2018-08-01 10:43:29 -0700

    Remove incorrect function dispatch

--- src/fields/nodes.jl
+++ src/fields/nodes.jl
@@ -62,7 +62,3 @@ end
 function (∘)(p::Nodes{T, NX, NY}, q::Nodes) where {T, NX, NY}
     product!(Nodes(T, (NX, NY)), p, q)
 end
-
-function (∘)(p::Array{T,2}, q::Array{T,2}) where {T}
-    p*q
-end
